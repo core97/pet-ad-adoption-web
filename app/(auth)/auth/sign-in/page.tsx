8679efddb730c372147b583d361a4e4c7e3809f4
Juan Cortes <j.cortes@crowdfarming.com>
2023-04-22 21:46:21 +0200

    chore: restructure app dir

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,3 @@
+const SignInPage = () => <h1>Sign in</h1>;
+
+export default SignInPage;
---
+++ app/(main)/page.tsx
@@ -0,0 +1,7 @@
+const HomePage = () => (
+  <main>
+    <h1>Home page</h1>
+  </main>
+);
+
+export default HomePage;
