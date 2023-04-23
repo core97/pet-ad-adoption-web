2dd075842c1f99a64b2a139f0c11d5248a49fed3
Juan Cortes <j.cortes@crowdfarming.com>
2023-04-23 19:36:20 +0200

    feat: create auth service

---
+++ modules/authentication/auth-service.ts
@@ -0,0 +1,33 @@
+import { fetcher } from 'modules/shared/application/fetcher';
+
+export const signIn = async (params: { email: string; password: string }) => {
+  const user = await fetcher(
+    `${process.env.NEXT_PUBLIC_API_URL}/users/sign-in`,
+    {
+      method: 'PUT',
+      body: JSON.stringify({ email: params.email, password: params.password }),
+    }
+  );
+
+  return user;
+};
+
+export const signUp = async (params: {
+  email: string;
+  name: string;
+  password: string;
+}) => {
+  const user = await fetcher(
+    `${process.env.NEXT_PUBLIC_API_URL}/users/sign-up`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email: params.email,
+        name: params.name,
+        password: params.password,
+      }),
+    }
+  );
+
+  return user;
+};
